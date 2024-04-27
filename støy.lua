2a539821d0097547220cf8862b2e92914f83980c
imminentgloom <67942961+imminentgloom@users.noreply.github.com>
2024-04-27 19:39:53 +0200

    Update støy.lua

--- støy.lua
+++ støy.lua
@@ -83,34 +83,34 @@ function init()
 	
 	params:add_separator("delay")
 	
-  params:add{id="delay_level", name="delay level", type="control", 
+ 	params:add{id="delay_level", name="delay level", type="control", 
     controlspec=controlspec.new(0,1,'lin',0,0.5,""),
     action=function(x) softcut.level(1,x) end}
   
-  params:add{id="delay_rate", name="delay rate", type="control", 
+  	params:add{id="delay_rate", name="delay rate", type="control", 
     controlspec=controlspec.new(0.1,20,'exp',0,0.1,""),
     action=function(x) softcut.rate(1,x) end}
   
-  params:add{id="delay_feedback", name="delay feedback", type="control", 
+  	params:add{id="delay_feedback", name="delay feedback", type="control", 
     controlspec=controlspec.new(0,1.0,'lin',0,0.75,""),
     action=function(x) softcut.pre_level(1,x) end}
   
-  params:add{id="delay_pan", name="delay pan", type="control", 
+  	params:add{id="delay_pan", name="delay pan", type="control", 
     controlspec=controlspec.new(-1,1.0,'lin',0,0,""),
     action=function(x) softcut.pan(1,x) end}
 	
-  audio.level_cut(1.0)
+	audio.level_cut(1.0)
 	audio.level_adc_cut(1)
 	audio.level_eng_cut(0)
-  softcut.level(1,1.0)
-  softcut.level_slew_time(1,0.25)
+  	softcut.level(1,1.0)
+  	softcut.level_slew_time(1,0.25)
 	softcut.level_input_cut(1, 1, 1.0)
 	softcut.level_input_cut(2, 1, 1.0)
 	softcut.pan(1, 0.0)
 
-  softcut.play(1, 1)
+  	softcut.play(1, 1)
 	softcut.rate(1, 1)
-  softcut.rate_slew_time(1,0.25)
+  	softcut.rate_slew_time(1,0.25)
 	softcut.loop_start(1, 1)
 	softcut.loop_end(1, 1.5)
 	softcut.loop(1, 1)
