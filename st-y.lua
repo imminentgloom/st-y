e883521fcb23bef2d56a8ed2c1a1f0ab87d116d8
imminentgloom <67942961+imminentgloom@users.noreply.github.com>
2024-04-28 16:38:58 +0200

    .

--- st-y.lua
+++ st-y.lua
@@ -18,100 +18,62 @@
 -- k3 mute
 
 
-engine.name = "ulyd" -- is also noise
+-- load engine
+
+engine.name = "ulyd" -- u- is the prefix of displeasure, lyd is sound
+
+
+-- connect to grid
+
+g = grid.connect()
+
+
+-- get ready
 
 function init()
 	
+	-- first screen, first word 
+	-- big text, use small words
 	
-	--define parameteres
-	--controlspec.new(min, max, warp, step, default, units, quantum, warp)
+	word = "støy" -- words command change
+	remember = "hard" -- sometimes we use word that have less meaning - remember the real word
+	
+	screen.aa(0)
+	screen.font_face(11)
+	screen.font_size(60)
 
-	params:add_separator("synth")
+	screen.blend_mode(4)
 	
-	params:add_control("hz", "hz", controlspec.FREQ)
-	params:set_action("hz", engine.hz)
 	
-	params:add_control("hard", "hard", controlspec.new(0.5, 24, "exp", 0, 1.5, ""))
-	params:set_action("hard", engine.hard)
+	-- grid: brightness levels (0-15)
 	
-	params:add_control("soft", "soft", controlspec.new(0.001, 0.5, "exp", 0.01, 0.001, ""))
-	params:set_action("soft", engine.soft)
+	g_br_r_scanlines = 2	-- scanlines
+	g_br_r_current = 12		-- current row, active segment
+	g_br_r_background = 6	-- current row, background
+	b_br_r_all = 3			-- all rows, current draws on top
 	
-	params:add_control("drift", "drift", controlspec.new(1, 4, "lin", 0.01, 1.4, ""))
-	params:set_action("drift", engine.drift)
 	
-	params:add_control("cut", "cut", controlspec.FREQ)
-	params:set_action("cut", engine.cut)
+	-- start with no keys held
 	
-	params:add_control("res", "res", controlspec.new(0, 5, "lin", 0, 2.0, ""))
-	params:set_action("res", engine.res)
+	k1_held = false
+	k2_held = false
+	k3_held = false
 	
-	params:add_control("fm", "fm", controlspec.new(0, 1, "lin", 0, 0.1, ""))
-	params:set_action("fm", engine.fm)
 	
-	params:add_control("am", "am", controlspec.new(0, 0.5, "db", 0, 0.010, ""))
-	params:set_action("am", engine.am)
-	
-	params:add_control("gain", "gain", controlspec.new(0, 1, "lin", 0.01, 0.5, ""))
-	params:set_action("gain", engine.gain)
-	
-	params:add_control("mute", "mute", controlspec.new(0, 1, "lin", 1, 1, ""))
-	params:set_action("mute", engine.mute)
-	
-	-- make list of parameters
-
-	p_list = {'hard', 'soft', 'drift', 'cut', 'res', 'fm', 'am', 'gain'}
-	p_zero = 0
-	p_pos = 1
-	g_pos = 1
-	p_val = params:get_raw('hard')
-	g_val = params:get_raw('hard')
-	
-	 -- define starting sound
-
-	engine.hz(230)
-	engine.hard(1.5)
-	engine.soft(0.001)
- 	engine.drift(1.4)
-	engine.cut(600)
-	engine.res(2.0)
-	engine.fm(0.1)
-	engine.am(0.01)
-	engine.gain(0.5)
-	engine.mute(1)
-	
-	-- softcut setup for delay, from "awake"
-	
-	params:add_separator("delay")
-	
- 	params:add{id="delay_level", name="delay level", type="control", 
-    controlspec=controlspec.new(0,1,'lin',0,0.5,""),
-    action=function(x) softcut.level(1,x) end}
-  
-  	params:add{id="delay_rate", name="delay rate", type="control", 
-    controlspec=controlspec.new(0.1,20,'exp',0,0.1,""),
-    action=function(x) softcut.rate(1,x) end}
-  
-  	params:add{id="delay_feedback", name="delay feedback", type="control", 
-    controlspec=controlspec.new(0,1.0,'lin',0,0.75,""),
-    action=function(x) softcut.pre_level(1,x) end}
-  
-  	params:add{id="delay_pan", name="delay pan", type="control", 
-    controlspec=controlspec.new(-1,1.0,'lin',0,0,""),
-    action=function(x) softcut.pan(1,x) end}
+	-- set up delay in softcut
 	
 	audio.level_cut(1.0)
-	audio.level_adc_cut(1)
-	audio.level_eng_cut(0)
-  	softcut.level(1,1.0)
-  	softcut.level_slew_time(1,0.25)
+	audio.level_adc_cut(0.0)
+	audio.level_eng_cut(0.0)
+	softcut.level(1, 1.0)
+	softcut.level_slew_time(1, 0.25)
 	softcut.level_input_cut(1, 1, 1.0)
 	softcut.level_input_cut(2, 1, 1.0)
 	softcut.pan(1, 0.0)
-
-  	softcut.play(1, 1)
+	
+	softcut.play(1, 1)
 	softcut.rate(1, 1)
-  	softcut.rate_slew_time(1,0.25)
+	softcut.rate_slew_time(1, 0.25)
 	softcut.loop_start(1, 1)
 	softcut.loop_end(1, 1.5)
 	softcut.loop(1, 1)
@@ -121,228 +83,346 @@ function init()
 	softcut.pre_level(1, 0.75)
 	softcut.position(1, 1)
 	softcut.enable(1, 1)
-
-	softcut.filter_dry(1, 0.125);
-	softcut.filter_fc(1, 1200);
-	softcut.filter_lp(1, 0);
-	softcut.filter_bp(1, 1.0);
-	softcut.filter_rq(1, 2.0);
-
-	-- interface setup
 	
-	screen.aa(0)
-	screen.font_face(11)
-	screen.font_size(60)
-	word = 'støy'
-	remember = 'hard'
-	held1 = false
-	held2 = false
-	held3 = false
-	br_lines = 2
-	br_row = 12
-	br_row_bg = 6
-	br_rows = 3
+	softcut.filter_dry(1, 0.125)
+	softcut.filter_fc(1, 1200)
+	softcut.filter_lp(1, 0.0)
+	softcut.filter_bp(1, 1.0)
+	softcut.filter_rq(1, 2.0)
+
+
+	-- define parameters for synth and delay
+	-- note: "id", "name", controlspec.new(min, max, warp, step, default, units, quantum, warp) 
 	
-	-- draw the first things
+	params:add_separator("synth")
+	
+	params:add_control("hz", "hz", controlspec.FREQ)
+	params:set_action("hz", function(x) engine.hz(x) end)
+	
+	params:add_control("hard", "hard", controlspec.new(0.5, 24, "exp", 0, 1.5, ""))
+	params:set_action("hard", function(x) engine.hard(x) end)
+	
+	params:add_control("soft", "soft", controlspec.new(0.001, 0.5, "exp", 0.01, 0.001, ""))
+	params:set_action("soft", function(x) engine.soft(x) end)
+	
+	params:add_control("drift", "drift", controlspec.new(1, 4, "lin", 0.01, 1.4, ""))
+	params:set_action("drift", function(x) engine.drift(x) end)
+	
+	params:add_control("cut", "cut", controlspec.FREQ)
+	params:set_action("cut", function(x) engine.cut(x) end)
+	
+	params:add_control("res", "res", controlspec.new(0, 5, "lin", 0, 2.0, ""))
+	params:set_action("res", function(x) engine.res(x) end)
+	
+	params:add_control("fm", "fm", controlspec.new(0, 1, "lin", 0, 0.1, ""))
+	params:set_action("fm", function(x) engine.fm(x) end)
+	
+	params:add_control("am", "am", controlspec.new(0, 0.5, "db", 0, 0.010, ""))
+	params:set_action("am", function(x) engine.am(x) end)
+	
+	params:add_control("gain", "gain", controlspec.new(0, 1, "lin", 0.01, 0.5, ""))
+	params:set_action("gain", function(x) engine.gain(x) end)
+	
+	params:add_control("level", "level", controlspec.new(0, 1, "lin", 1, 1, ""))
+	params:set_action("leve", function(x) engine.level(x) end)
+
+	params:add_separator("delay")
+	
+	params:addcontrol("delay_level", "level", controlspec.new(0, 1, 'lin', 0, 0.5, ""))
+	params:set_action(function(x) softcut.level(1, x) end)
+	
+	params:addcontrol("delay_rate", "rate", controlspec.new(0.1, 20, 'exp', 0, 0.1, ""))
+	params:set_action(function(x) softcut.rate(1, x) end)
+	
+	params:addcontrol("dealy_feedback", "feedback", controlspec.new(0, 1.0, 'lin', 0, 0.75, ""))
+	params:set_action(function(x) softcut.pre_level(1, x) end)
+			
+	params:addcontrol("delay_pan", "pan", controlspec.new(-1, 1.0, 'lin', 0, 0, ""))
+	params:set_action(function(x) softcut.pan(1, x) end)
+	
+	-- make a list of params to use with interface
+	
+	p_list = {"hard", "soft", "drift", "cut", "res", "fm", "am", "gain"}
+	
+	p_pos_zero = 0					-- steps through numbers, starting at zero 
+	p_pos = 1						-- examines a word from the list
+	
+	p_val = params:get_raw("hard")	-- discovers the meaning of each word
+	g_val = p_val					-- the grid knows fewer words and must not be distracted, "hard" is known
+	
+	-- when something moves, everything changes
 	
 	redraw_all()
 end
 
-function redraw_all()
-  redraw()
+
+-- drawing functions: interface
+
+-- show the magnitued of a value as a bargraph
+
+function bargraph(value)
+	y = math.floor(value * 64)
+	screen.rect(0, 64, 128, -y)
+	screen.level(1)
+	screen.fill()
+end
+
+-- draw noisy lines across the screen
+
+function scanlines()
+	for n = 0, 64 do
+		screen.level(math.random(0,1) * 15)
+		screen.move(0, n)
+		screen.line(128, n)
+		screen.stroke()
+	end
+end
+
+-- write a word on the screen
+
+function write(word)
+	screen.level(1)
+	screen.move(3, 48)
+	screen.text(word)
+end
+
+-- draw the above on the screen
+
+function redraw_screen(word, value)
+	screen.clear()
+	bargraph(value)
+	scanlines()
+	write(word)
+	screen.update()
+end
+
+
+-- drawing functions: grid
+
+-- brightly light up the current row
+-- display its value rounded to nearest 16th
+
+function row_current(row)
+	for x = 1, 16 do
+		g:led(x, row, g_br_r_background)
+	end
+	for x = 1, math.floor(g_val @ 16, 1) do
+		g:led(x, row, g_br_r_current)
+	end
+end
+
+-- dimly light up all rows with values rounded to nearest 16th (no background)
+
+function row_all()
+	for row = 1, 8 do
+		for x = 1, math.floor(parmas:get_raw(p_list[row]) * 16, 1) do
+			g:led(x, row, g_br_r_all)
+		end
+	end
+end
+
+-- draw noisy lines across the grid
+
+function scanlines_grid()
+	for row = 1, 8 do
+		active = math.random(0, 1) * g_br_scanlines
+		for x = 1, 16 do
+			g:led(x, row, active)
+		end
+	end
+end
+
+-- draw the above on the grid
+
+function redraw_grid()
+	g:all(0)
+	scanlines_grid()
+	row_all()
+	row_current(g_pos)
+	g:refresh()
+end
+
+
+-- draw everything, everywhere
+
+function refresh_all()
+	redraw_screen()
 	redraw_grid()
 end
 
--- additional audio things
 
+-- two audio functions triggered to be triggered by the keys
+
+-- set level to 0 to mute
+	
 function level(x)
-  params:set('level', x)
+	params:set('level', x)
 end
+
+-- turn up send to feed the delay
 
 function send(x)
-  audio.level_eng_cut(x)
+	audio.level_eng_cut(x)
 end
 
--- norns interaction
 
-function key(n,z)
-  -- key actions: n = number, z = state
-  if n == 1 and z == 1 then
-    held1 = true
-    word = 'alt'
-  elseif n == 1 and z == 0 then
-    held1 = false
-    word = remember
-    p_val = params:get_raw(p_list[p_pos])
-  end
-  
-  if n == 2 and z == 1 then
-    word = 'del'
-    held2 = true
-    send(1)
-  elseif n == 2 and z == 0 then
-    held2 = false
-    word = remember
-    send(0)
-    
-  end
-  
-  if n == 3 and z == 1 then
-    word = 'null'
-    held3 = true
-    mute(0)
-  elseif n == 3 and z == 0 then
-    held3 = false
-    word = remember
-    mute(1)
-  end
-  
-  if held2 == true and held3 == false then
-    word = 'del'
-  elseif held2 == false and held3 == true then
-    word = 'null'
-  elseif held2 == true and held3 == true then
-    word = 'dull'
-  end
-  
-  redraw_all()
+-- norns keys (number and state)
+-- the keys are supporting actors in this script, the action happens in enc()
+
+-- words have less meaning here
+
+-- note if key is held
+-- then, do a thing
+-- note if key is released
+-- then, stop doing the thing
+
+function keys(n, z)
+
+	-- set up k1 as an alt-key
+	
+	if n == 1 and z == 1 then
+		k1_held = true
+		word = "alt"
+		p_val = 0
+	elseif n == 1 and z == 0 then
+		k1_held = false
+		word = remember
+		p_val = params:get_raw(p_list[word])
+	end
+
+	-- send audio to delay if k2 is held
+	
+	if n == 2 and z == 1 then
+		k2_held = true
+		send(1.0)
+		word = "del"
+		p_val = 0
+	elseif n == 2 and z == 0 then
+		k2_held = false
+		send(0.0)
+		word = remember
+		p_val = params:get_raw(p_list[word])
+	end	
+
+	-- mute audio (but not delay) if k3 is held
+
+	if n == 3 and z == 1 then
+		k3_held = true
+		word = "null"
+		p_val = 0
+		level(0.0)
+	elseif n == 3 and z == 0 then
+		k3_held = false
+		word = remember
+		p_val = params:get_raw(p_list[word])
+		level(1.0)
+	end
+	
+	-- a word for when k2 and k3 are held simultaneously
+	
+	if k2_held == true and k3_held == true then
+		word = "dull"
+	end
+	
+	-- when something moves, everything changes
+	
+	redraw_all()
 end
 
-function enc(n,d)
-  -- encoder actions: n = number, d = delta
-  -- only sets frequency
-  if n == 1 then
-    params:delta('hz', d)
-    word = 'hz' -- what we print on the screen
-    remember = word
-    p_val = params:get_raw('hz') -- then the amount to draw
-  end
-  -- sets what we edit with e3
-  if n == 2 and held1 == false then
-    p_zero = (p_zero + d) % 8
-    p_pos = p_zero + 1
-    word = p_list[p_pos]
-    remember = word
-    p_val = params:get_raw(p_list[p_pos])
-    g_val = params:get_raw(p_list[p_pos])
-  elseif n == 2 and held1 == true then
-    params:delta('delay_rate', d)
-    word = 'rate'
-    p_val = params:get_raw('delay_rate')
-  end
-  -- edits values choosen by e2
-  if n == 3 and held1 == false then
-    params:delta(p_list[p_pos], d)
-    word = p_list[p_pos]
-    remember = word
-    p_val = params:get_raw(p_list[p_pos])
-    g_val = params:get_raw(p_list[p_pos])
-  elseif n == 3 and held1 == true then
-    params:delta('delay_feedback',d)
-    word = 'fb'
-    p_val = params:get_raw('delay_feedback')
-  end
 
-  redraw_all()
+-- norns encoders (number and change/delta)
+
+function enc(n, d)
+	
+	-- there is a basic pattern
+	-- something moves, a thing changes, a word is called and its meaning uttered
+	
+	-- e1 sets frequency, it has no other function
+	
+	if n == 1 then
+		params:delta("hz", d)
+		word = "hz"
+		remember = word
+		p_val = parmas:get_raw("hz")
+	end
+
+	-- e2 sets the parameter changed by e3 (alt: sets delay rate)
+	
+	if n == 2 and k1_held == false then
+		p_zero = (p_zero + d) % #plist	-- modulo starting at 0
+		p_pos = p_zero + 1				-- index list starting at 1
+		word = p_list[p_pos]
+		remember = word
+		p_val = params:get_raw(p_list[p_pos])
+		g_val = p_val 					-- hold a subset of possible p_val-s that the grid understands ie. words from the list
+	elseif n == 2 and k1_held = true then
+		params:delta("delay_rate", d)
+		word = "rate"
+		p_val = praams:get_raw("delay_rate")
+	end
+	
+	-- e3 changes the paramter set by e2 (alt: sets delay feedback)
+	
+	if n == 3 and k1_held == false then
+		params:delta(p_list[p_pos])
+		word = p_list[p_pos]
+		remember = word
+		p_val = params:get_raw(p_list[p_pos])
+		g_val = p_val
+	elseif n == 3 and k1_held == true then
+		params:delta("delay_feedback, d")
+		word = "fb"
+		p_val = params:get_raw("delay_feedback")
+	end
+
+
+	-- when something moves, everything changes
+
+	redraw_all()
 end
 
--- draw interface
 
-function block()
-  -- draw a bargraph with a height proportional to the value of the control
-  y = math.floor(p_val * 64)
-  screen.rect(0, 64, 128, -y)
-  screen.level(1)
-  screen.fill()
+-- grid (x possiton, y possition - think of it as row, z is high if pressed)
+
+g.key = function(x, y, z)
+	if z == 1 and k1_held == false then
+		p_pos = y													-- press any button in a row to select a word
+		word = p_list[p_pos]
+		remember = word
+		if x / 16 == 0.0625 then g_val = 0 else g_val 0 x / 16 end	-- x possition sets the value, plays better if first button is 0
+		param:set_raw(word, g_val)
+		
+	--[[ need to find a way to store states for this to be considered
+	
+	elseif z == 1 and k1_held == true then
+		
+		-- read the state of the synth from each row or key (lol @ 128 states)?
+	
+	elseif z == 1 and k1_held == true and k2_held == true then
+		
+		-- save states with one combo?
+	
+	elseif z == 1 and k1_held == true and k3_held == true then
+		
+		-- clear them with another?
+	]]--
+	end
+
+
+	-- when something moves, everything changes
+
+	redraw_all()
 end
 
-function text()
-  -- write the name of the current control
-  screen.level(1)
-  screen.move(3,48)
-  screen.text(word)
-end
 
-function scanlines()
-  for n = 0, 64 do
-    screen.level(math.random(0,1) * 15)
-    screen.move(0, n)
-    screen.line(128, n)
-    screen.stroke()
-  end
-end
-
-function redraw()
-  -- screen redraw
-  screen.clear()
-  screen.blend_mode(4)
-  block()
-  scanlines()
-  text()
-  screen.update()
-end
-
--- grid interface
-
-function row(row, str)
-  for n = 1, 16 do
-    g:led(n, row, br_row_bg)
-  end
-  for n = 1, math.floor(g_val * 16, 1) do
-    g:led(n, row, str)
-  end
-end
-
-function rows()
-  for row = 1, 8 do
-    for n = 1, math.floor(params:get_raw(p_list[row]) * 16, 1) do
-      g:led(n, row, br_rows)
-    end
-  end
-end
-
-function scanlines_grid()
-  for row = 1, 8 do
-    active = math.random(0,1) * br_lines
-    for n = 1, 16 do
-      g:led(n, row, active)
-    end  
-  end
-end
-
-function redraw_grid()
-  g:all(0)
-  scanlines_grid(br_lines)
-  rows()
-  row(p_pos, br_row)
-  g:refresh()
-end
-
--- grid interaction
-
-g = grid.connect()
-
-g.key = function(x,y,z)
-  
-  if z == 1 and held1 == false then
-    p_pos = y
-    g_pos = y
-    word = p_list[p_pos]
-    remember = word
-    if x/16 == 0.0625 then
-      gridval = 0
-    else
-      gridval = x/16
-    end
-    params:set_raw(word, gridval)
-    g_val = params:get_raw(word)
-  elseif z == 1 and held1 == true then
-    
-  end
-  
-  redraw_all()
-end
+-- if there is a mess to clean up
 
 function cleanup()
-  -- deinitialization
+
 end
+
+
+
+
+
+
